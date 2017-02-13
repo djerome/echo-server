7ed4cf27984b08539954e688e3f136eb8aa9b011
Dan Jerome <djerome@gmail.com>
2017-02-13 17:25:46 -0500

    Cleaned up log messages

--- echo_server.py
+++ echo_server.py
@@ -45,7 +45,6 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
 server_address = ('', 10000)
 logger.info('Starting up on {} port {}'.format(*server_address))
-print('Starting up on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
 # Listen for incoming connections
@@ -55,24 +54,20 @@ sock.listen(5)
 while True:
 	connection, client_address = sock.accept()
 	client_ip = client_address[0]
+	port = client_address[1]
 	client_fqdn = socket.gethostbyaddr(client_ip)
-	print 'IP = ' + client_ip + ', FQDN = ' + client_fqdn[0]
+	log_msg = 'Received connect from ' + client_fqdn[0] + ' (' + client_ip + '); local port ' + str(port) + ')'
+	logger.info(log_msg)
 	try:
-		print('Connection from ' + str(client_address))
 
 		# Receive the data in small chunks and
 		# retransmit it
 		while True:
 			data = connection.recv(receive_chunk)
-			print('received {!r}'.format(data))
 			if data:
-				print('Sending data back to ' + str(client_address))
 				connection.sendall(data)
 			else:
-				logger.warn('No data from {0} ({1})'.format(client_fqdn, client_ip))
-				print('No data from ' + str(client_address))
 				break
 	finally:
 		# Clean # up # the # connection
 		connection.close()
-		print('Closing connection from ' + str(client_address))
